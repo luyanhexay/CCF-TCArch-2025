2820a0d3c8e37c956fe3710249d8cee4248f31e7
afluren <2874200933@qq.com>
2025-09-09 22:42:39 +0800

    加上了特判小样例（高度优化）
    但也可以看出现有算法并没有发挥出GPU的并发能力？

--- hip_programming_contest/prefix_sum/main.cpp
+++ hip_programming_contest/prefix_sum/main.cpp
@@ -1,16 +1,20 @@
 #include "main.h"
+#include <chrono>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         std::cerr << "usage: " << argv[0] << " <input_file>" << std::endl;
         return 1;
     }
-    
+
     std::ifstream input_file;
     std::string filename = argv[1];
-    
+
     input_file.open(filename);
-    if (!input_file.is_open()) {
+    if (!input_file.is_open())
+    {
         std::cerr << "fileopen error " << filename << std::endl;
         return 1;
     }
@@ -19,13 +23,20 @@ int main(int argc, char* argv[]) {
     input_file >> N;
 
     std::vector<int> input(N), output(N);
-    for(int i = 0; i < N; ++i) 
+    for (int i = 0; i < N; ++i)
         input_file >> input[i];
     input_file.close();
 
+    // auto start = std::chrono::high_resolution_clock::now();
+    solve(input.data(), output.data(), N);
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    // std::cout << "Kernel time: " << duration.count() << "us" << std::endl;
     solve(input.data(), output.data(), N);
 
-    for(int i = 0; i < N; ++i) 
+    for (int i = 0; i < N; ++i)
         std::cout << output[i] << " ";
+    // printf("%d ", output[i]);
+    // std::cout << std::endl;
     std::cout << std::endl;
 }
