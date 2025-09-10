b1202285694fe570eeb35952cbf09c70ee713002
luyanhexay <https://github.com/luyanhexay>
2025-09-10 06:32:24 +0000

    store: fail transplate

--- hip_programming_contest/apsp-refactor/blocked_floyd_warshall.h
+++ hip_programming_contest/apsp-refactor/blocked_floyd_warshall.h
@@ -1,14 +1,25 @@
-#ifndef TYPEDEF_ERROR
-#define TYPEDEF_ERROR
+#ifndef BLOCKED_FLOYD_WARSHALL_H
+#define BLOCKED_FLOYD_WARSHALL_H
+
 #include "errors.h"
+
+#ifdef __cplusplus
+extern "C"
+{
 #endif
 
-/**
- * @param graph input graph
- * @param n number of graph's vertices
- * @param b block size, (0 < b <= n)
- * @param apsp all pairs shortest paths
- * @param err in case of error during the execution, this will contain some info about it
- * @return 0 if success, non-zero otherwise
- */
-int blocked_floyd_warshall(int *graph, int n, int b, int *apsp, t_error *err);
+    /**
+     * @param graph input graph
+     * @param n number of graph's vertices
+     * @param b block size, (0 < b <= n)
+     * @param apsp all pairs shortest paths
+     * @param err in case of error during the execution, this will contain some info about it
+     * @return 0 if success, non-zero otherwise
+     */
+    int blocked_floyd_warshall(int *graph, int n, int b, int *apsp, t_error *err);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BLOCKED_FLOYD_WARSHALL_H
